8bf8fc9bd37242d9c4f48ce98145be1ac303ea1f
Scott Dn <scott.dn91@gmail.com>
2022-04-08 05:00:42 +0700

    feat: vitest

---
+++ src/App.test.tsx
@@ -0,0 +1,17 @@
+import {describe, expect, it} from 'vitest';
+
+import App from './App';
+import {customRender, screen, userEvent} from './testUtils';
+
+describe('Simple working test', () => {
+  it('the title is visible', () => {
+    customRender(<App />);
+    expect(screen.getByText(/Hello Vite \+ React!/i)).toBeInTheDocument();
+  });
+
+  it('should increment count on click', async () => {
+    customRender(<App />);
+    userEvent.click(screen.getByRole('button'));
+    expect(await screen.findByText(/count is: 1/i)).toBeInTheDocument();
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
+/// <reference types="vitest" />
+
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts'
+  }
 });
