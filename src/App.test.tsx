4dc5c50af174c47eef7f6015b26fcf802f852281
Scott Dn <scott.dn91@gmail.com>
2022-04-11 20:56:10 +0700

    chore: no need import React

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import {describe, expect, it} from 'vitest';
 
 import App from './App';
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import {StrictMode} from 'react';
 
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
+  </StrictMode>,
   document.getElementById('root')
 );
