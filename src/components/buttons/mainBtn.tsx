9e880d6335fb953e23e7073ae2461d1666458d36
Scott Dn <scott.dn91@gmail.com>
2022-04-12 21:25:08 +0700

    chore: remove storybook

--- src/components/buttons/mainBtn.tsx
+++ src/components/buttons/mainBtn.tsx
@@ -2,7 +2,7 @@ import {colors, darkColors, customStitches} from '@/themes';
 
 const {styled} = customStitches;
 
-const Flex = styled('div', { display: 'flex' });
+const Flex = styled('div', {display: 'flex'});
 
 const Button = styled('button', {
   all: 'unset',
@@ -23,21 +23,21 @@ const Button = styled('button', {
         color: colors.slate7,
         boxShadow: `0 2px 10px ${colors.red3}`,
         '&:hover': {backgroundColor: colors.purple5},
-        '&:focus': {boxShadow: `0 0 0 2px ${colors.slate3}`},
+        '&:focus': {boxShadow: `0 0 0 2px ${colors.slate3}`}
       },
       green: {
         backgroundColor: darkColors.red1,
         color: darkColors.slate7,
         boxShadow: `0 2px 10px ${darkColors.red3}`,
         '&:hover': {backgroundColor: darkColors.purple5},
-        '&:focus': {boxShadow: `0 0 0 2px ${darkColors.slate3}`},
-      },
-    },
+        '&:focus': {boxShadow: `0 0 0 2px ${darkColors.slate3}`}
+      }
+    }
   },
 
   defaultVariants: {
-    variant: 'violet',
-  },
+    variant: 'violet'
+  }
 });
 
 export const MainBtn = () => (
