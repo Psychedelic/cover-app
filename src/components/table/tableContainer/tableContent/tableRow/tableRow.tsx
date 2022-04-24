8a6f446087c0370fe419a48e943320d090cb788f
Scott Dn <scott.dn91@gmail.com>
2022-04-24 14:01:59 +0700

    feat: verification table collapse button

--- src/components/table/tableContainer/tableContent/tableRow/tableRow.tsx
+++ src/components/table/tableContainer/tableContent/tableRow/tableRow.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
+import {faCaretSquareDown, faCaretSquareRight} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {CSS, VariantProps} from '@stitches/react';
 
 import {Core} from '@/components';
@@ -11,15 +13,36 @@ interface PropTypes extends React.ComponentProps<typeof StitchesTableRow> {
   type?: VariantProps<typeof Core.Dot>;
   children: React.ReactElement[];
   override?: boolean;
+  showCollapseBtn?: boolean;
+  onCollapse?: (id: string) => void;
+  isSelected?: boolean;
+  rowId?: string;
 }
 
-export const TableRow: React.FC<PropTypes> = ({css, children, type, override}) => (
-  <StitchesTableRow css={css}>
-    {type && (
-      <td>
-        <Core.Dot type={type} />
-      </td>
-    )}
-    {children.map(c => (override ? c : <td key={c.key}>{c}</td>))}
-  </StitchesTableRow>
-);
+export const TableRow: React.FC<PropTypes> = ({
+  css,
+  children,
+  type,
+  override,
+  showCollapseBtn,
+  isSelected,
+  onCollapse,
+  rowId
+}) => {
+  const onClick = useCallback(() => onCollapse && onCollapse(rowId as string), [rowId, onCollapse]);
+  return (
+    <StitchesTableRow css={css}>
+      {type && (
+        <td>
+          <Core.Dot type={type} />
+        </td>
+      )}
+      {children.map(c => (override ? c : <td key={c.key}>{c}</td>))}
+      {showCollapseBtn && (
+        <Core.Button onClick={onClick} type={'text'}>
+          <FontAwesomeIcon icon={isSelected ? faCaretSquareDown : faCaretSquareRight} size={'lg'} />
+        </Core.Button>
+      )}
+    </StitchesTableRow>
+  );
+};
--- src/parts/verificationTable/verificationTable.tsx
+++ src/parts/verificationTable/verificationTable.tsx
@@ -1,9 +1,5 @@
 import React, {useCallback, useState} from 'react';
 
-import {faCaretSquareDown} from '@fortawesome/free-regular-svg-icons';
-import {faCaretSquareRight} from '@fortawesome/free-solid-svg-icons';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-
 import {Core, TableContainer, TableContent, TableHeader, TableRow} from '@/components';
 
 import {tableBodyStyle, tableHeaderStyle} from './verificationTable.styled';
@@ -22,40 +18,40 @@ interface PropTypes {
   verifications?: Verification[];
 }
 
-// FIXME: react-hooks/rules-of-hooks
 export const VerificationTable: React.VFC<PropTypes> = ({verifications}) => {
   const [canisterIdSelected, setCanisterIdSelected] = useState('');
+  const onCollapse = useCallback(
+    (canisterId: string) => {
+      setCanisterIdSelected(canisterId === canisterIdSelected ? '' : canisterId);
+    },
+    [canisterIdSelected]
+  );
   return (
     <TableContainer>
       <TableHeader css={tableHeaderStyle}>
         {['Canister ID', 'Name', 'Repo', 'Git Commit', 'Wasm Hash', 'Last Verified', '']}
       </TableHeader>
       <TableContent css={tableBodyStyle}>
-        {verifications?.map(({isVerified, canisterId, name, repo, gitCommit, wasmHash, lastVerified}) => {
-          const onClick = useCallback(() => {
-            setCanisterIdSelected(canisterIdSelected === canisterId ? '' : canisterId);
-          }, [canisterIdSelected, canisterId]);
-          return (
-            <TableRow key={canisterId} type={isVerified ? 'green' : 'red'}>
-              <Core.CopyableText key={0}>{canisterId}</Core.CopyableText>
-              <span key={1}>{name}</span>
-              <span key={2}>{repo}</span>
-              <Core.CopyableText color={'gray'} key={3}>
-                {gitCommit}
-              </Core.CopyableText>
-              <Core.CopyableText color={'gray'} key={4}>
-                {wasmHash}
-              </Core.CopyableText>
-              <span key={5}>{lastVerified}</span>
-              <Core.Button key={6} onClick={onClick} type={'text'}>
-                <FontAwesomeIcon
-                  icon={canisterIdSelected === canisterId ? faCaretSquareDown : faCaretSquareRight}
-                  size={'lg'}
-                />
-              </Core.Button>
-            </TableRow>
-          );
-        })}
+        {verifications?.map(({isVerified, canisterId, name, repo, gitCommit, wasmHash, lastVerified}) => (
+          <TableRow
+            isSelected={canisterIdSelected === canisterId}
+            key={canisterId}
+            onCollapse={onCollapse}
+            rowId={canisterId}
+            showCollapseBtn
+            type={isVerified ? 'green' : 'red'}>
+            <Core.CopyableText key={0}>{canisterId}</Core.CopyableText>
+            <span key={1}>{name}</span>
+            <span key={2}>{repo}</span>
+            <Core.CopyableText color={'gray'} key={3}>
+              {gitCommit}
+            </Core.CopyableText>
+            <Core.CopyableText color={'gray'} key={4}>
+              {wasmHash}
+            </Core.CopyableText>
+            <span key={5}>{lastVerified}</span>
+          </TableRow>
+        ))}
       </TableContent>
     </TableContainer>
   );
