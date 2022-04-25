b4a4a289aaf26515f76b7760ae0e461f121f7892
Scott Dn <scott.dn91@gmail.com>
2022-04-25 22:09:17 +0700

    feat: verification details

--- src/parts/verificationTable/verificationTable.styled.ts
+++ src/parts/verificationTable/verificationTable.styled.ts
@@ -1,3 +1,33 @@
+export const tableContainerStyle = {
+  '& tbody': {
+    '& tr:nth-of-type(n+2)': {
+      borderTop: '1px solid $coverSilver'
+    },
+    '& td:nth-of-type(3)': {
+      color: '$coverLightWhite'
+    },
+    '& td:nth-of-type(4)': {
+      color: '$coverLightWhite',
+      textDecoration: 'underline'
+    },
+    '& td:nth-of-type(8)': {
+      textAlign: 'center'
+    },
+    '& svg': {
+      cursor: 'pointer'
+    },
+    '& td button': {
+      padding: '$0'
+    }
+  }
+};
+
+export const tableContentTransparent = {
+  '& td': {
+    opacity: 0.3
+  }
+};
+
 export const tableHeaderStyle = {
   '& th': {
     '&:last-of-type': {
@@ -6,28 +36,76 @@ export const tableHeaderStyle = {
   }
 };
 
-export const tableBodyStyle = {
-  /*
-   * '& td': {
-   * opacity: 0.3
-   * },
-   */
-  '& tr:nth-of-type(n+2)': {
+export const tableRowSelected = {
+  background: '$coverInk',
+  '& td': {
+    opacity: 1
+  },
+  '+ tr td div': {
     borderTop: '1px solid $coverSilver'
   },
-  '& td:nth-of-type(3)': {
-    color: '$coverLightWhite'
+  '+ tr, + tr + tr, + tr + tr + tr, + tr + tr + tr + tr': {
+    border: 'none !important',
+    background: '$coverInk',
+    '& td': {
+      opacity: 1,
+      height: '1px',
+      textDecoration: 'none !important',
+      '& div': {
+        height: '100%',
+        width: '100%',
+        display: 'inline-flex',
+        alignItems: 'center'
+      }
+    },
+    '& td:nth-of-type(odd) div': {
+      color: '$coverLightGray'
+    },
+    '& td:nth-of-type(even) div': {
+      color: '$coverLightWhite',
+      justifyContent: 'flex-end'
+    }
   },
-  '& td:nth-of-type(4)': {
-    color: '$coverLightWhite'
+  '+ tr': {
+    '& td:nth-of-type(1) div span': {
+      padding: '25px 0px 10px 25px'
+    },
+    '& td:nth-of-type(2) div span': {
+      padding: '25px 50px 10px 0px'
+    },
+    '& td:nth-of-type(3) div span': {
+      padding: '25px 0px 10px 0px'
+    },
+    '& td:nth-of-type(4) div span': {
+      padding: '25px 50px 10px 0px'
+    }
   },
-  '& td:nth-of-type(8)': {
-    textAlign: 'center'
+  '+ tr + tr, + tr + tr + tr': {
+    '& td:nth-of-type(1) div span': {
+      padding: '10px 0px 10px 25px'
+    },
+    '& td:nth-of-type(2) div span': {
+      padding: '10px 50px 10px 0px'
+    },
+    '& td:nth-of-type(3) div span': {
+      padding: '10px 0px'
+    },
+    '& td:nth-of-type(4) div span': {
+      padding: '10px 50px 10px 0px'
+    }
   },
-  '& svg': {
-    cursor: 'pointer'
-  },
-  '& td button': {
-    padding: '$0'
+  '+ tr + tr + tr + tr': {
+    '& td:nth-of-type(1) div span': {
+      padding: '10px 0px 25px 25px'
+    },
+    '& td:nth-of-type(2) div span': {
+      padding: '10px 50px 25px 0px'
+    },
+    '& td:nth-of-type(3) div span': {
+      padding: '10px 0px 25px 0px'
+    },
+    '& td:nth-of-type(4) div span': {
+      padding: '10px 50px 25px 0px'
+    }
   }
 };
--- src/parts/verificationTable/verificationTable.tsx
+++ src/parts/verificationTable/verificationTable.tsx
@@ -2,7 +2,13 @@ import React, {useCallback, useState} from 'react';
 
 import {Core, TableContainer, TableContent, TableHeader, TableRow} from '@/components';
 
-import {tableBodyStyle, tableHeaderStyle} from './verificationTable.styled';
+import {DetailCell} from './DetailCell';
+import {
+  tableContainerStyle,
+  tableContentTransparent,
+  tableHeaderStyle,
+  tableRowSelected
+} from './verificationTable.styled';
 
 interface Verification {
   isVerified: boolean;
@@ -12,6 +18,14 @@ interface Verification {
   gitCommit: string;
   wasmHash: string;
   lastVerified: string;
+  ownerId: string;
+  repoVisibility: string;
+  rustVersion: string;
+  canisterType: string;
+  dfxVersion: string;
+  optimizeCount: string;
+  buildWasmHash: string;
+  buildUrl: string;
 }
 
 interface PropTypes {
@@ -27,63 +41,71 @@ export const VerificationTable: React.VFC<PropTypes> = ({verifications}) => {
     [canisterIdSelected]
   );
   return (
-    <TableContainer>
+    <TableContainer css={tableContainerStyle} paginated>
       <TableHeader css={tableHeaderStyle}>
         {['Canister ID', 'Name', 'Repo', 'Git Commit', 'Wasm Hash', 'Last Verified', '']}
       </TableHeader>
-      <TableContent css={tableBodyStyle}>
-        {verifications?.map(({isVerified, canisterId, name, repo, gitCommit, wasmHash, lastVerified}) => (
-          <React.Fragment key={canisterId}>
-            <TableRow
-              isSelected={canisterIdSelected === canisterId}
-              onCollapse={onCollapse}
-              rowId={canisterId}
-              showCollapseBtn
-              type={isVerified ? 'green' : 'red'}>
-              <Core.CopyableText key={0}>{canisterId}</Core.CopyableText>
-              <span key={1}>{name}</span>
-              <span key={2}>{repo}</span>
-              <Core.CopyableText color={'gray'} key={3}>
-                {gitCommit}
-              </Core.CopyableText>
-              <Core.CopyableText color={'gray'} key={4}>
-                {wasmHash}
-              </Core.CopyableText>
-              <span key={5}>{lastVerified}</span>
-            </TableRow>
-            {canisterIdSelected === canisterId && (
-              <>
-                <TableRow override>
-                  <td />
-                  <td colSpan={3}>
-                    <span>{'1'}</span>
-                  </td>
-                  <td colSpan={4}>
-                    <span>{'2'}</span>
-                  </td>
-                </TableRow>
-                <TableRow override>
-                  <td />
-                  <td colSpan={3}>
-                    <span>{'3'}</span>
-                  </td>
-                  <td colSpan={4}>
-                    <span>{'4'}</span>
-                  </td>
-                </TableRow>
-                <TableRow override>
-                  <td />
-                  <td colSpan={3}>
-                    <span>{'5'}</span>
-                  </td>
-                  <td colSpan={4}>
-                    <span>{'6'}</span>
-                  </td>
-                </TableRow>
-              </>
-            )}
-          </React.Fragment>
-        ))}
+      <TableContent css={canisterIdSelected === '' ? undefined : tableContentTransparent}>
+        {verifications?.map(
+          ({
+            isVerified,
+            canisterId,
+            name,
+            repo,
+            gitCommit,
+            wasmHash,
+            lastVerified,
+            ownerId,
+            repoVisibility,
+            rustVersion,
+            canisterType,
+            dfxVersion,
+            optimizeCount,
+            buildWasmHash,
+            buildUrl
+          }) => (
+            <React.Fragment key={canisterId}>
+              <TableRow
+                css={canisterIdSelected === canisterId ? tableRowSelected : undefined}
+                isSelected={canisterIdSelected === canisterId}
+                onCollapse={onCollapse}
+                rowId={canisterId}
+                showCollapseBtn
+                type={isVerified ? 'green' : 'red'}>
+                <Core.CopyableText key={0}>{canisterId}</Core.CopyableText>
+                <span key={1}>{name}</span>
+                <span key={2}>{repo}</span>
+                <Core.CopyableText color={'gray'} key={3}>
+                  {gitCommit}
+                </Core.CopyableText>
+                <Core.CopyableText color={'gray'} key={4}>
+                  {wasmHash}
+                </Core.CopyableText>
+                <span key={5}>{lastVerified}</span>
+              </TableRow>
+              {canisterIdSelected === canisterId && (
+                <>
+                  <TableRow override>
+                    <DetailCell label={'Owner Principal ID'} value={ownerId} />
+                    <DetailCell label={'Repo visibility'} value={repoVisibility} />
+                  </TableRow>
+                  <TableRow override>
+                    <DetailCell label={'Rust version'} value={rustVersion} />
+                    <DetailCell label={'Canister type'} value={canisterType} />
+                  </TableRow>
+                  <TableRow override>
+                    <DetailCell label={'DFX version'} value={dfxVersion} />
+                    <DetailCell label={'WASM Optimization count'} value={optimizeCount} />
+                  </TableRow>
+                  <TableRow override>
+                    <DetailCell label={'Build WASM hash'} value={buildWasmHash} />
+                    <DetailCell label={'Build Result'} value={buildUrl} />
+                  </TableRow>
+                </>
+              )}
+            </React.Fragment>
+          )
+        )}
       </TableContent>
     </TableContainer>
   );
