1eedafb947dc5e41790f3cb4930808583bc5dba5
Scott Dn <scott.dn91@gmail.com>
2022-05-02 22:29:26 +0700

    feat: page header dispatch search verification by canister id

--- src/parts/pageContainer/pageHeader/pageHeader.tsx
+++ src/parts/pageContainer/pageHeader/pageHeader.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 
 import {Link} from 'react-router-dom';
 
 import {logo} from '@/assets';
 import {Core, SearchBar} from '@/components';
 import {DASHBOARD_PATH, SUBMIT_PATH} from '@/constants';
+import {fetchVerifications, getByCanisterId, useVerificationContext} from '@/contexts';
 import {getCurrentPath} from '@/utils';
 
 import {StitchesPageHeaderContainer, StitchesPageMainHeader, StitchesPageSecondaryHeader} from './pageHeader.styled';
 
-export const PageHeader: React.VFC = () => (
-  <StitchesPageHeaderContainer>
-    <StitchesPageMainHeader>
-      <Link to={DASHBOARD_PATH}>
-        <img alt={'logo'} src={logo} />
-      </Link>
-      <SearchBar />
-      <Core.Button size={'medium'} type={'text'}>
-        {'Blog'}
-      </Core.Button>
-      <Core.Button size={'medium'} type={'text'}>
-        {'Docs'}
-      </Core.Button>
-    </StitchesPageMainHeader>
-    <StitchesPageSecondaryHeader>
-      <Link to={SUBMIT_PATH}>
-        <Core.Button disabled={getCurrentPath() === SUBMIT_PATH}>{'Submit Verification'}</Core.Button>
-      </Link>
-      <Core.Button>{'Connect to Plug'}</Core.Button>
-    </StitchesPageSecondaryHeader>
-  </StitchesPageHeaderContainer>
-);
+export const PageHeader: React.VFC = () => {
+  const {dispatch} = useVerificationContext();
+  const onBlur = useCallback(
+    value => {
+      value ? getByCanisterId(dispatch, value) : fetchVerifications(dispatch);
+    },
+    [dispatch]
+  );
+  return (
+    <StitchesPageHeaderContainer>
+      <StitchesPageMainHeader>
+        <Link to={DASHBOARD_PATH}>
+          <img alt={'logo'} src={logo} />
+        </Link>
+        <SearchBar onBlur={onBlur} />
+        <Core.Button size={'medium'} type={'text'}>
+          {'Blog'}
+        </Core.Button>
+        <Core.Button size={'medium'} type={'text'}>
+          {'Docs'}
+        </Core.Button>
+      </StitchesPageMainHeader>
+      <StitchesPageSecondaryHeader>
+        <Link to={SUBMIT_PATH}>
+          <Core.Button disabled={getCurrentPath() === SUBMIT_PATH}>{'Submit Verification'}</Core.Button>
+        </Link>
+        <Core.Button>{'Connect to Plug'}</Core.Button>
+      </StitchesPageSecondaryHeader>
+    </StitchesPageHeaderContainer>
+  );
+};
