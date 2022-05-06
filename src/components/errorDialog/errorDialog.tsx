3a8456b56c3905d4d7fdb521e4375a45ac4294d8
Scott Dn <scott.dn91@gmail.com>
2022-05-06 20:08:58 +0700

    feat: error dialog handler

--- src/components/errorDialog/errorDialog.tsx
+++ src/components/errorDialog/errorDialog.tsx
@@ -5,51 +5,68 @@ import {Core} from '@/components';
 import {StitchesBtnGroup} from './errorDialog.styled';
 
 interface PropTypes {
+  cancelContent?: string;
   actionContent?: string;
   open?: boolean;
-  onAction?: React.ReactEventHandler;
+  onAction?: () => void;
 }
 
 export interface ErrorDialogHandler {
-  open: () => void;
+  open: (option?: {title?: string; description?: React.ReactNode; showActionBtn?: boolean}) => void;
 }
 
 export const ErrorDialog = React.forwardRef<ErrorDialogHandler, PropTypes>(
-  ({actionContent = 'Retry', open, onAction}, ref) => {
+  ({cancelContent = 'Cancel', actionContent = 'Retry', open, onAction}, ref) => {
     const [isOpen, setIsOpen] = useState(open);
+    const [showActionBtn, setShowActionBtn] = useState(false);
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState<React.ReactNode>();
     useImperativeHandle(ref, () => ({
-      open: () => {
+      open: (option = {title: '', description: '', showActionBtn: false}) => {
         setIsOpen(true);
+        setTitle((option.title as string) && ` ${option.title}`);
+        setDescription(option.description);
+        setShowActionBtn(Boolean(option.showActionBtn));
       }
     }));
     const onCancelClick = useCallback(() => {
       setIsOpen(false);
     }, []);
+    const onActionClick = useCallback(() => {
+      setIsOpen(false);
+      onAction && onAction();
+    }, [onAction]);
     return (
       <Core.AlertDialog open={isOpen}>
         <Core.AlertDialogContent>
           <Core.AlertDialogTitle>
             <Core.Dot size={'large'} type={'red'} />
-            {' We ran into an issue'}
+            {title || ' We ran into an issue'}
           </Core.AlertDialogTitle>
           <Core.AlertDialogDescription>
-            {'Please retry the submission, if this issue persists report in our'}{' '}
-            <a href={'https://discord.gg/yVEcEzmrgm'} rel={'noreferrer'} target={'_blank'}>
-              {'Discord'}
-            </a>
-            {'.'}
+            {description || (
+              <>
+                {'Please retry the submission, if this issue persists report in our '}
+                <a href={'https://discord.gg/yVEcEzmrgm'} rel={'noreferrer'} target={'_blank'}>
+                  {'Discord'}
+                </a>
+                {'.'}
+              </>
+            )}
           </Core.AlertDialogDescription>
           <StitchesBtnGroup>
             <Core.AlertDialogCancel asChild>
               <Core.Button onClick={onCancelClick} size={'large'} type={'outline'}>
-                {'Cancel'}
+                {cancelContent}
               </Core.Button>
             </Core.AlertDialogCancel>
-            <Core.AlertDialogAction asChild>
-              <Core.Button onClick={onAction} size={'large'} type={'main'}>
-                {actionContent}
-              </Core.Button>
-            </Core.AlertDialogAction>
+            {showActionBtn && (
+              <Core.AlertDialogAction asChild>
+                <Core.Button onClick={onActionClick} size={'large'} type={'main'}>
+                  {actionContent}
+                </Core.Button>
+              </Core.AlertDialogAction>
+            )}
           </StitchesBtnGroup>
         </Core.AlertDialogContent>
       </Core.AlertDialog>
