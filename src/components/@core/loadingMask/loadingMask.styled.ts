23194cdf094dac4b42fa83d32044af5eabc66b07
Scott Dn <scott.dn91@gmail.com>
2022-05-09 23:44:02 +0700

    feat: loading mask blink effect

--- src/components/@core/loadingMask/loadingMask.styled.ts
+++ src/components/@core/loadingMask/loadingMask.styled.ts
@@ -1,9 +1,16 @@
 import {defaultStitches} from '@/themes';
-const {styled} = defaultStitches;
+const {styled, keyframes} = defaultStitches;
+
+const blink = keyframes({
+  '0%': {opacity: 1},
+  '50%': {opacity: 0.5},
+  '100%': {opacity: 1}
+});
 
 export const StitchesLoadingMask = styled('div', {
   borderRadius: '$mask',
   background: '$coverGradientGray',
+  animation: `${blink} 2s linear infinite;`,
   variants: {
     size: {
       dot: {
