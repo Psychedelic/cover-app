50a391ffd3c4458209ef1f74da689b09219d4a4b
Scott Dn <scott.dn91@gmail.com>
2022-06-23 22:31:24 +0700

    fix: linter + npm script

--- gitInfo.js
+++ gitInfo.js
@@ -1,17 +1,13 @@
 const {execSync} = require('child_process');
 const fs = require('fs');
-const path = require('path');
 
 const execSyncWrapper = command => execSync(command).toString().trim();
 const gitBranch = execSyncWrapper('git rev-parse --abbrev-ref HEAD');
 const gitCommitHash = execSyncWrapper('git rev-parse HEAD');
 
-const obj = {
+const gitInfo = {
   gitBranch,
   gitCommitHash
 };
 
-const filePath = path.resolve('src/components/@core/gitInfo', 'generatedGitInfo.json');
-const fileContents = JSON.stringify(obj, null, 2);
-
-fs.writeFileSync(filePath, `${fileContents}\n`);
+fs.writeFileSync('./src/components/@core/gitInfo/generatedGitInfo.json', `${JSON.stringify(gitInfo, null, 2)}\n`);
