4618bc869ad80a3a99e0925a1f3e5937788de11e
Scott Dn <scott.dn91@gmail.com>
2022-07-18 21:52:08 +0700

    feat: authentication context (#28)
    * feat: authentication context
    
    * chore: pump deps
    
    * chore: fix build

---
+++ src/contexts/authenticationContext.ts
@@ -0,0 +1,79 @@
+import {Dispatch, ReducerAction, useContext} from 'react';
+
+import {Principal} from '@dfinity/principal';
+
+import {ActionBase, createContext, createProvider} from './helper';
+
+/*
+ * ========================================================================================================
+ * UTILS
+ * ========================================================================================================
+ */
+
+/*
+ * ========================================================================================================
+ * ACTION INTERFACES
+ * ========================================================================================================
+ */
+export interface AuthenticationInfo {
+  isAuthenticated?: boolean;
+  pid?: Principal;
+}
+type Action = AuthenticationAction;
+interface AuthenticationAction extends ActionBase {
+  type: 'authenticationAction';
+  payload: {
+    pid: Principal;
+  };
+}
+
+/*
+ * ========================================================================================================
+ * STATE
+ * ========================================================================================================
+ */
+interface State {
+  authenticationInfo?: AuthenticationInfo;
+}
+const INIT_STATE: State = {};
+
+/*
+ * ========================================================================================================
+ * CONTEXT
+ * ========================================================================================================
+ */
+const context = createContext<State, Action>(INIT_STATE);
+export const useAuthenticationContext = () => useContext(context);
+
+/*
+ * ========================================================================================================
+ * REDUCER
+ * ========================================================================================================
+ */
+const authenticationReducer = (_: State, action: Action): State => {
+  if (action.type === 'authenticationAction') {
+    return {
+      authenticationInfo: {
+        isAuthenticated: true,
+        pid: action.payload.pid
+      }
+    };
+  }
+  throw new Error(`Unhandled action type: ${(action as ActionBase).type}`);
+};
+
+/*
+ * ========================================================================================================
+ * PROVIDER
+ * ========================================================================================================
+ */
+export const AuthenticationProvider = createProvider(context, authenticationReducer, INIT_STATE);
+
+/*
+ * ========================================================================================================
+ * ACTIONS
+ * ========================================================================================================
+ */
+export const authenticate = (dispatch: Dispatch<ReducerAction<typeof authenticationReducer>>, pid: Principal) => {
+  dispatch({type: 'authenticationAction', payload: {pid}});
+};
