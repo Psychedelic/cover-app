54fc98bc9143bc3ffd5a02fcee24b4e9786f7d92
Scott Dn <scott.dn91@gmail.com>
2022-10-29 06:43:45 +0700

    style: loading icon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,6 @@
 import emptyActivity from './emptyActivity.png';
 import logo from './logo.svg';
 import notFound from './notFound.svg';
+import loading from './loading.png';
 
-export {logo, notFound, emptyActivity};
+export {logo, notFound, emptyActivity, loading};
