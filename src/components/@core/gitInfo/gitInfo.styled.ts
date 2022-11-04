12a75f55dd29fc603904c4320e29b132e76e31f0
Scott Dn <scott.dn91@gmail.com>
2022-11-05 05:01:39 +0700

    chore: improve gitInfo

--- src/components/@core/gitInfo/gitInfo.styled.ts
+++ src/components/@core/gitInfo/gitInfo.styled.ts
@@ -4,7 +4,7 @@ const {styled} = defaultStitches;
 export const StitchesGitInfo = styled('div', {
   position: 'fixed',
   bottom: '0.5%',
-  right: '0.5%',
+  left: '0.5%',
   color: '$coverWhiteOpacity',
   fontSize: '10px'
 });
--- src/components/@core/gitInfo/gitInfo.tsx
+++ src/components/@core/gitInfo/gitInfo.tsx
@@ -1,15 +1,34 @@
+import {useCallback, useEffect, useState} from 'react';
+
 import {gitBranch, gitCommitHash} from './generatedGitInfo.json';
 import {StitchesGitInfo} from './gitInfo.styled';
 
-export const GitInfo = () => (
-  <StitchesGitInfo>
-    <div>
-      <span>
-        {'Build version: '}
-        {gitBranch}
-        {'/'}
-        {gitCommitHash}
-      </span>
-    </div>
-  </StitchesGitInfo>
-);
+export const GitInfo = () => {
+  const [windowSize, setWindowSize] = useState({height: window.innerHeight, width: window.innerWidth});
+
+  const isDesktop = windowSize.width >= 1260 && windowSize.height >= 820;
+
+  const updateWindowSize = useCallback(() => {
+    setWindowSize({height: window.innerHeight, width: window.innerWidth});
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('resize', updateWindowSize);
+    return () => {
+      window.removeEventListener('resize', updateWindowSize);
+    };
+  }, [updateWindowSize]);
+
+  return isDesktop ? (
+    <StitchesGitInfo>
+      <div>
+        <span>
+          {'Build version: '}
+          {gitBranch}
+          {'/'}
+          {gitCommitHash}
+        </span>
+      </div>
+    </StitchesGitInfo>
+  ) : null;
+};
