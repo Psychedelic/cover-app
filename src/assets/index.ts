093b0f0d283629efec35bf33a37b994c66679d80
Scott Dn <scott.dn91@gmail.com>
2022-11-05 04:49:02 +0700

    feat: desktop only

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,8 +1,8 @@
+import desktopBottomLeftCorner from './desktopBottomLeftCorner.png';
+import desktopTopRightCorner from './desktopTopRightCorner.png';
 import emptyActivity from './emptyActivity.png';
 import loading from './loading.png';
 import logo from './logo.svg';
 import notFound from './notFound.svg';
-import desktopTopRightCorner from './desktopTopRightCorner.png';
-import desktopBottomLeftCorner from './desktopBottomLeftCorner.png';
 
 export {logo, notFound, emptyActivity, loading, desktopTopRightCorner, desktopBottomLeftCorner};
