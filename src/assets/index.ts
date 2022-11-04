0a6a30c0f90fd463446f26bdb8325544d9e87d8f
Scott Dn <scott.dn91@gmail.com>
2022-11-05 02:57:31 +0700

    assets: desktopCorner

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,6 @@ import emptyActivity from './emptyActivity.png';
 import loading from './loading.png';
 import logo from './logo.svg';
 import notFound from './notFound.svg';
+import desktopCorner from './desktopCorner.png';
 
-export {logo, notFound, emptyActivity, loading};
+export {logo, notFound, emptyActivity, loading, desktopCorner};
